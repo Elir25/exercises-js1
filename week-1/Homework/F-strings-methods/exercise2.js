a0fc23ef015c79477942d1a5059fb3c25682f340
Elir25 <elirendiles@gmail.com>
2021-02-20 12:04:04 +0100

    I solved the exercise 2 in a better way

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,6 +1,12 @@
-const name = " Daniel  ";
+/*const name = " Daniel  ";
 let namelength = name.trim().length;
 const message = `hello my name is ${name.trim()} and my name is ${namelength} character long`;
-//console.log(name);
+
+console.log(message);*/
+let name = " Daniel  ";
+name = name.trim();
+let namelength = name.length;
+
+const message = `hello my name is ${name} and my name is ${namelength} character long`;
 console.log(message);
-//console.log(message);
+//console.log(name.trim);
