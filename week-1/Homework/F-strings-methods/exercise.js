195f9b1bb72f34ac008a488736ccf2d20f9b2403
Elir25 <elirendiles@gmail.com>
2021-02-20 10:45:56 +0100

    exercises homework

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
+let message = "Elialba";
+let messagelength = message.length;
 
 console.log(message);
+console.log(messagelength);
+console.log(`My name is ${message} and my name is 7 characters long`);
--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
 const name = " Daniel  ";
-
+let namelength = name.trim().length;
+const message = `hello my name is ${name.trim()} and my name is ${namelength} character long`;
+//console.log(name);
 console.log(message);
+//console.log(message);
