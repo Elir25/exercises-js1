63d0b9d6c42ee53cdf56116d87b437cb4a1354af
Elir25 <elirendiles@gmail.com>
2021-03-01 15:21:12 +0100

    Exercises I done

--- week-2/Homework/I-array-properties/exercise.js
+++ week-2/Homework/I-array-properties/exercise.js
@@ -6,8 +6,12 @@
 */
 
 function isEmpty(arr) {
-  return; // complete this statement
-}
+// complete this statement
+  if (arr.length === 0) {
+    return true;
+  } return false;
+  
+} 
 
 /* 
   DO NOT EDIT BELOW THIS LINE
--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -5,7 +5,7 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
-for (let i = 4; i < 20; i++) {
+for (let i = 0; i < 20; i++) {
   if (isEven(i + 1)) {
   console.log(`the exponential of ${i + 1} is ${exponential(i + 1)}`);
   }
