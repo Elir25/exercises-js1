156b0a2320224bd84d61879a1ce28012cbe5be44
Elir25 <elirendiles@gmail.com>
2021-03-03 07:53:47 +0100

    changes made to ex L

--- week-2/Homework/L-for-loop/exercise.js
+++ week-2/Homework/L-for-loop/exercise.js
@@ -20,11 +20,3 @@ function sumTillNum(num){
 console.log("Sum from 0 to " + n + " is: " + sumTillNum(n));
 
 
-/*4 ----> 10
-0 +1 + 2 + 3 + 4 = 10
-0 +1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
-primera vuelta i vale 0 es menor que 4? si, sumale 1
-segunda vuelta i vale 1 es menor que 4? si, sumale 1
-tercera vuelta i vale 2 es menor que 4? si, sumale 1
-4ta vuelta i vale 3 es menor que 4? si, sumale 1
-*/
